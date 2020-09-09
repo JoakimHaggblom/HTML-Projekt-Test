2e2c3721d8041dea2874e0a0dfbee554f0808a33
JoakimHaggblom <70258157+JoakimHaggblom@users.noreply.github.com>
2020-09-09 11:29:33 +0200

    Create main.js

---
+++ Scripts/main.js
@@ -0,0 +1,13 @@
+define HTMLElement
+
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+
+let bruh = 'ooga booga';
+if(bruh ==='ooga booga'){
+    alert('ooga booga ooga booga');
+    else {
+        alert('goo goo gaa gaa');
+    }
+}
